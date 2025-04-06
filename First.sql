fcda961bf93bea35f160be975a9c04566b9d64b4
SukhadxTomar <sukhadtomar@gmail.com>
2025-04-07 03:04:02 +0530

    having,aliasing,limit,group by order by

--- First.sql
+++ First.sql
@@ -1,6 +1,6 @@
 -- Select clause
 Select *
-from sakila.actor;
+from customer;
 Select * from sakila.actor;
 -- Where clause
 Select * from sakila.actor where first_name="nick";
@@ -12,4 +12,17 @@ where title like'%a' and title like 'a%';
 select first_name,min(actor_id)
 from actor 
 group by first_name;
+-- Order by
+select * from customer
+order by store_id asc;
+-- having clause
+SELECT store_id, AVG(store_id) AS avg_store
+FROM customer
+GROUP BY store_id
+HAVING store_id > 0;
+-- limit and aliasing
+select * 
+from actor limit 1,2;
+
+
 
